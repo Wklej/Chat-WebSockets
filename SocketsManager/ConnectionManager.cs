76d0c2d15a3f7e9f82f7d923b4ca1312df7ebe2b
Wklej <59125969+Wklej@users.noreply.github.com>
2021-06-01 20:13:19 +0200

    init

---
+++ SocketsManager/ConnectionManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chat.SocketsManager
+{
+    public class ConnectionManager
+    {
+        private ConcurrentDictionary<string, WebSocket> connections = new ConcurrentDictionary<string, WebSocket>();
+
+        public WebSocket getSocketById(string id_)
+        {
+            return connections.FirstOrDefault(x => x.Key == id_).Value;
+        }
+
+        public ConcurrentDictionary<string,WebSocket> getAllConnections()
+        {
+            return connections;
+        }
+
+        public string getID(WebSocket webSocket_)
+        {
+            return connections.FirstOrDefault(x => x.Value == webSocket_).Key;
+        }
+
+        public async Task RemoveSocketAsync(string id)
+        {
+            connections.TryRemove(id, out var socket);
+            await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Socket connection closed.", CancellationToken.None);
+        }
+
+        public void addSocket(WebSocket webSocket)
+        {
+            connections.TryAdd(getConnectionID(), webSocket);
+        }
+
+        public string getConnectionID()
+        {
+            return Guid.NewGuid().ToString("N");
+        }
+
+    }
+}
---
+++ SocketsManager/SocketHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chat.SocketsManager
+{
+    public abstract class SocketHandler
+    {
+        public ConnectionManager Connections { get; set; }
+
+        public SocketHandler(ConnectionManager connectionManager)
+        {
+            Connections = connectionManager;
+        }
+
+        public virtual async Task onConnected(WebSocket webSocket)
+        {
+            await Task.Run(() => { Connections.addSocket(webSocket); });
+        }
+
+        public virtual async Task onDisconnected(WebSocket webSocket)
+        {
+            await Connections.RemoveSocketAsync(Connections.getID(webSocket));
+        }
+
+        public async Task sendMessage(WebSocket webSocket, string message)
+        {
+            if (webSocket.State != WebSocketState.Open)
+                return;
+
+            await webSocket.SendAsync(new System.ArraySegment<byte>(Encoding.ASCII.GetBytes(message), 0, message.Length)
+                , WebSocketMessageType.Text, true, CancellationToken.None);
+
+        }
+
+    }
+}
